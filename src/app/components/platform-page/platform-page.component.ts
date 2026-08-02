46e21022793630e48cea6cf60eaea2544efb49f5
Hayden Westfall <westfallhayden@gmail.com>
2026-08-01 21:15:16 -0400

    optimizes this site for optimal SEO performance

--- src/app/components/platform-page/platform-page.component.ts
+++ src/app/components/platform-page/platform-page.component.ts
@@ -15,16 +15,19 @@ export class PlatformPageComponent implements OnInit {
       description:
         'Miss Lisa is inspired by anyone with a disability who refuses to allow  that particular disability to interfere with advancing in life. Her Illustrator is a perfect example of this!',
       image: '../../../../assets/img/child-learning-1.webp',
+      alt: 'Child overcoming a disability while learning',
     },
     {
       description:
         'The book series is intended for struggling readers who benefit from repetition of text, along with the simple enjoyment of a true childhood story.',
       image: '../../../../assets/img/child-learning-2.webp',
+      alt: 'Child practicing reading with repetitive text',
     },
     {
       description:
         'Miss Lisa is motivated by Intervention Specialists all over the world who can’t help but believe that all children can learn to read.',
       image: '../../../../assets/img/child-learning-3.webp',
+      alt: 'Child learning to read with the support of an intervention specialist',
     },
   ];
 
